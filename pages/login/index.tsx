84c79db85c645862eb631020f5cc422c2e418cb7
simpleneeraj <simplneeraj@gmail.com>
2022-10-10 12:23:41 +0530

    changes

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -6,48 +6,48 @@ import React from "react";
 import css from "styles/auth.module.scss";
 
 const LoginPage = () => {
-    const [email, setEmail] = React.useState("");
-    const [password, setPassword] = React.useState("");
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
 
-    const { mutate } = useLogin();
+  const { mutate } = useLogin();
 
-    const handleSubmit = (e: React.ChangeEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        mutate(
-            { email, password },
-            {
-                onSuccess: () => {
-                    Router.push("/");
-                },
-            }
-        );
-    };
-
-    return (
-        <div className={css.container}>
-            <form className={css.login} onSubmit={handleSubmit}>
-                <div className={css.heading}>
-                    <h2>Login as Admin</h2>
-                </div>
-                <Input
-                    type="text"
-                    placeholder="Enter your email"
-                    label={"Email"}
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                />
-                <Input
-                    type="password"
-                    label={"Password"}
-                    placeholder="Enter your password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                />
-                <div className={css.controls}>
-                    <Button>Submit</Button>
-                </div>
-            </form>
-        </div>
+  const handleSubmit = (e: React.ChangeEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    mutate(
+      { email, password },
+      {
+        onSuccess: () => {
+          Router.push("/");
+        },
+      }
     );
+  };
+
+  return (
+    <div className={css.container}>
+      <form className={css.login} onSubmit={handleSubmit}>
+        <div className={css.heading}>
+          <h2>Login as Admin</h2>
+        </div>
+        <Input
+          type="text"
+          placeholder="Enter your email"
+          label={"Email"}
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <Input
+          type="password"
+          label={"Password"}
+          placeholder="Enter your password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <div className={css.controls}>
+          <Button>Submit</Button>
+        </div>
+      </form>
+    </div>
+  );
 };
 export default LoginPage;
